666f42ba049e0d69e8f5d722dff2826ebfc795e8
yan <158830490+ballight42@users.noreply.github.com>
2025-05-28 16:34:14 -0700

    Update human.py with adopt()
            Simulate a cat being adopted!
    
            In the Owner class in human.py, add a function adopt(Cat) that adds the Cat object to the list of cats
    
            Write docstrings for this function, including a minimal example

--- pyCatSim/api/human.py
+++ pyCatSim/api/human.py
@@ -60,3 +60,62 @@ class Owner:
 
         self.name = name
         self.cats_owned = cats_owned
+
+    def adopt(self, cats_object):
+
+        """
+        2025.05.28 by Yan Pan
+        Simulate a cat being adopted!
+
+        In the Owner class in human.py, add a function adopt(Cat) that adds the Cat object to the list of cats
+
+        Write docstrings for this function, including a minimal example
+
+        Write a test
+
+        Parameters
+        ----------
+        cats_object: the cat to be added to the list
+
+        Raises
+        ------
+        TypeError
+                If any of the arguments are not Cat.
+        ValueError
+                If cats_object is not Cat.
+
+        Examples
+        --------
+
+        .. jupyter-execute::
+
+            import pyCatSim as cats
+
+            cat1 = cats.Cat(name="Whiskers")
+            cat2 = cats.Cat(name="Boots", color="tabby")
+            owner1 = Owner(name="Sasha", cats_owned=cat1)
+            owner2 = Owner(name="Liam", cats_owned=[cat1, cat2])
+
+            chestnut = cats.Cat(name='Chestnut', age = 4, color = 'tabby')
+            nutmeg = cats.Cat(name='Nutmeg', age = 3, color = 'tortoiseshell')
+            owner1.adopt(owner1,nutmeg)
+            owner2.adopt(owner2,[chestnug,nutmeg])
+
+            print(owner1.name)
+            print([cat.name for cat in owner2.cats_owned])
+
+
+        """
+        if isinstance(cats_object, Cat):
+            self.cats_owned.append(cats_object)
+        elif isinstance(cats_object, list):
+            if not all(isinstance(cat, Cat) for cat in cats_object):
+                raise TypeError("All elements in cats_object must be instances of Cat.")
+            else:
+                self.cats_owned+=cats_object
+        else:
+            raise TypeError("cats_owned must be a Cat instance or a list of Cat instances.")
+
+
+
+
