40408d469fbb573c74cc24a6ce05a3038ecc55e5
jeanmico <costellojeanm@gmail.com>
2025-05-29 16:52:27 -0400

    docs update

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.1',
+    VERSION: '2025.5.27',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
---
+++ mytemp.py
@@ -0,0 +1,4 @@
+import pyCatSim as cats
+import random as random
+nutmeg = cats.Cat(name="Nutmeg", color = "Black")
+print(nutmeg.make_noise('random'))
